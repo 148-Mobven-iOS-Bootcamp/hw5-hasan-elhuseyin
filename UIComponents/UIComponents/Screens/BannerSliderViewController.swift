0dc552ca66f24f6ca093ab3d0d608e685cf49d26
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2022-01-22 00:50:31 +0300

    Add BannerSlider homework code

---
+++ UIComponents/UIComponents/Screens/BannerSliderViewController.swift
@@ -0,0 +1,89 @@
+//
+//  BannerSliderViewController.swift
+//  UIComponents
+//
+//  Created by Hasan Elhussein on 21.01.2022.
+//
+
+import UIKit
+
+class BannerSliderViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageController: UIPageControl!
+    
+    //MARK: - Variables
+    let arrOfLabels: [String] = ["Banner 1", "Banner 2", "Banner 3", "Banner 4", "Banner 5"]
+    var timer : Timer?
+    var currentCellIndex: Int = 0
+    
+    //MARK: - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Connect collectionView delegate and datasource to self
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        // Configure pageController
+        pageController.numberOfPages = arrOfLabels.count
+        
+        // Start the timer function
+        startTimer()
+    }
+    
+    //MARK: - Functions
+    // Function to slide the banner every 10 seconds
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(moveToNextIndex), userInfo: nil, repeats: true)
+    }
+    
+    // Objective-C function for startTimer
+    @objc func moveToNextIndex() {
+        if currentCellIndex < arrOfLabels.count - 1 {
+            currentCellIndex += 1
+        } else {
+            currentCellIndex = 0
+        }
+        // Scroll function
+        collectionView.scrollToItem(at: IndexPath(item: currentCellIndex, section: 0), at: .centeredHorizontally, animated: true)
+        // Set pageController to the proper index
+        pageController.currentPage = currentCellIndex
+    }
+    
+}
+
+
+//MARK: - Extensions
+
+extension BannerSliderViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // Return the number of cells
+        return arrOfLabels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Create a cell of type 'BannerSliderCell'
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerSliderCell", for: indexPath) as! BannerSliderCell
+        // Configure cell's attributes
+        cell.label.text = arrOfLabels[indexPath.row]
+        cell.view.backgroundColor = UIColor.random
+        // Return the cell
+        return cell
+    }
+    
+}
+
+extension BannerSliderViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // Set the size of the cells programmatically
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        // Set the spacing between cells to 0
+        return 0
+    }
+    
+}
